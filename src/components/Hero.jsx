c176760137de12b54a55821f65f3a7e8f0ff1a7c
sachin2709 <sachinmishra2709@gmail.com>
2025-05-21 13:31:22 +0530

    modified resume

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 import Tilt from "react-parallax-tilt";
 // import codingGif from "../assets/download.gif";
-import resumePDF from '../assets/sachin2709resume.pdf';
+import resumePDF from '../assets/sachin2709resume1.pdf';
 
 const Hero = () => {
   return (
